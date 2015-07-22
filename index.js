24a2380d4b5bf34cf2a31b262e2675122f325218
Sean Wright <s.galen.wright@gmail.com>
2015-07-21 23:17:27 -0400

    3. class vs functions

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
 (function () {
 	'use strict';
 
-	usingConst();
-	
-	function usingConst() {
-		const year = "2015";
+	usingFunction();
+	usingClass();
+
+	function usingFunction() {
+		console.log("Using a function to create an object");
+		function Bear() {
+			if (this) {
+				this.color = "Brown";
+				console.log("Inside a " + this.color + " bear");
+
+				this.sayHello = function () {
+					console.log("Hello!");
+				}
+			} else {
+				console.log("Inside a bear");
+			}
+		}
+
+		// 'this' does not exist inside Bear() with this call because
+		// no 'new' operator was used
+		Bear();
+
+		let brownBear = new Bear();
+
+		console.log(brownBear.color);
+		brownBear.sayHello();
+	}
+
+	function usingClass() {
+		console.log("Using a class to create an object");
+		class Bear {
+			constructor() {
+				this.color = "Brown";
+
+				console.log("Inside a " + this.color + " bear");
+			}
+
+			sayHello() {
+				console.log("Hello!");
+			}
+		}
 		
-		year = 2016; // Not allowed since year is a const
+		// Cannot call class constructors without 'new' keyword
+		// Bear();
+		
+		let brownBear = new Bear();
+
+		console.log(brownBear.color);
+		brownBear.sayHello();
 	}
 } ());
